242529dc03b5d69b73538417c25b57a729573bd5
Shota FUJI <pockawoooh@gmail.com>
2024-05-01 13:19:49 +0900

    Add logging options for docs build script
    Sometime I need full debug log, sometime I only need build time stats.
    Or I could just want to know whether the build succeeds.

--- docs/build.ts
+++ docs/build.ts
@@ -4,6 +4,7 @@
 
 import * as log from "../deps/deno.land/std/log/mod.ts";
 import * as cli from "../deps/deno.land/std/cli/mod.ts";
+import * as colors from "../deps/deno.land/std/fmt/colors.ts";
 
 import { DenoFsReader } from "../filesystem_reader/deno_fs.ts";
 import { DenoFsWriter } from "../filesystem_writer/deno_fs.ts";
@@ -74,16 +75,145 @@ export async function build() {
 	});
 }
 
+function prettyLogFormatter(
+	useColors: boolean,
+	verbose: boolean,
+): log.FormatterFunction {
+	function id<T>(x: T): T {
+		return x;
+	}
+
+	function pipe<T>(a: (x: T) => T, b: (x: T) => T): (x: T) => T {
+		return (x) => b(a(x));
+	}
+
+	const ts = useColors ? pipe(colors.black, colors.dim) : id;
+
+	return (record) => {
+		let level: (str: string) => string = id;
+		let msg: (str: string) => string = id;
+		let payload: (str: string) => string = id;
+
+		if (useColors) {
+			switch (record.level) {
+				case log.LogLevels.NOTSET:
+					level = colors.dim;
+					msg = colors.dim;
+					payload = colors.dim;
+					break;
+				case log.LogLevels.DEBUG:
+					level = colors.gray;
+					msg = colors.gray;
+					payload = pipe(colors.dim, colors.black);
+					break;
+				case log.LogLevels.INFO:
+					level = colors.blue;
+					msg = pipe(colors.bold, colors.black);
+					payload = colors.black;
+					break;
+				case log.LogLevels.WARN:
+					level = colors.yellow;
+					msg = colors.black;
+					payload = colors.black;
+					break;
+				case log.LogLevels.ERROR:
+					level = colors.red;
+					msg = colors.black;
+					payload = colors.black;
+					break;
+				case log.LogLevels.CRITICAL:
+					level = pipe(colors.red, colors.bold);
+					msg = pipe(colors.bold, colors.red);
+					payload = colors.red;
+					break;
+			}
+		}
+
+		let ret = "\n" + level(record.levelName) + " " +
+			ts(record.datetime.toISOString()) + "\n" +
+			msg(record.msg);
+
+		if (verbose) {
+			for (const arg of record.args) {
+				const text = Deno.inspect(arg, {
+					colors: useColors,
+					compact: false,
+				});
+
+				for (const line of text.split("\n")) {
+					ret += "\n    " + payload(line);
+				}
+			}
+		}
+
+		return ret;
+	};
+}
+
 if (import.meta.main) {
 	const args = cli.parseArgs(Deno.args, {
-		boolean: ["json"],
+		string: ["log"],
+		boolean: ["json", "verbose", "help"],
+		alias: {
+			"help": ["h"],
+		},
 	});
+
+	const useColors = Deno.stdout.isTerminal();
+
+	if (args.help) {
+		const title = useColors
+			? (s: string) => colors.underline(colors.bold(s))
+			: (s: string) => s;
+
+		console.log(`
+docs/build.ts
+
+Build Macana's documentation website using Macana.
+
+${title("Usage")}:
+    deno run --allow-read=docs --allow-write=docs/.dist docs/build.ts [OPTIONS]
+
+${title("Options")}:
+    -h, --help
+        Print this help text to stdout and exit.
+
+    --log=<debug|info|warn|error|critical>
+        Set the lowest log level to output.
+        By default, docs/build.ts logs INFO, WARN, ERROR, and CRITICAL level logs.
+
+    --json
+        Output logs as JSON Lines.
+
+    --verbose
+        Output additional information alongisde log messages.
+        This does not take effect when ${colors.bold("--json")} is set.
+		`.trim());
+		Deno.exit(0);
+	}
+
+	let level: log.LevelName = "INFO";
+	if (args.log) {
+		const upper = args.log.toUpperCase() as log.LevelName;
+		const found = log.LogLevelNames.includes(upper);
+		if (found) {
+			level = upper;
+		} else {
+			log.critical(
+				`"${args.log}" is not a valid log level.\n` +
+					` Available log levels are: ${log.LogLevelNames.join(", ")}`,
+			);
+			Deno.exit(1);
+		}
+	}
 
 	log.setup({
 		handlers: {
-			default: new log.ConsoleHandler("DEBUG", {
-				formatter: args.json ? log.jsonFormatter : undefined,
-				useColors: Deno.stdout.isTerminal(),
+			default: new log.ConsoleHandler(level, {
+				formatter: args.json
+					? log.jsonFormatter
+					: prettyLogFormatter(useColors, args.verbose),
+				useColors,
 			}),
 		},
 		loggers: {
